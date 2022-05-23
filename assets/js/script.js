2c0bfcd151fba8b9ca4c182e58de3fe5626280f8
carterwsheppard <carterwsheppard@gmail.com>
2022-05-22 21:50:01 -0400

    initial js psuedocode and variable / function setup with event listener added

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//Declare variables to select the history area and weather container so that we can append things to those
+var weatherContainer = document.querySelector("#weather-container");
+var searchHistoryContainer = document.querySelector("#search-history");
+var cityForm = document.querySelector("#city-form");
+
+//Add a function for btnHandler
+var btnHandler = function() {
+
+
+
+}
+
+
+//Event Listener to btnClick 
+cityForm.addEventListener("submit", btnHandler);
