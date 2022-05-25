e98ee86e531e3629e4f65d2066432d8fb1a0754e
carterwsheppard <carterwsheppard@gmail.com>
2022-05-25 19:39:45 -0400

    btnHandler is getting api response from weather app

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,49 @@
 var weatherContainer = document.querySelector("#weather-container");
 var searchHistoryContainer = document.querySelector("#search-history");
 var cityForm = document.querySelector("#city-form");
+var cityFormInput = document.querySelector("#city")
 
 //Add a function for btnHandler
 var btnHandler = function(event) {
     event.preventDefault();
 
-console.log("function is listening!")
+    //define location variable
+    var location = cityFormInput.value.trim().toLowerCase();
+
+    if (location) {
+        getLocationWeather(location);
+    
+        // clear old content
+        cityFormInput.textContent = "";
+        cityFormInput.value = "";
+      } else {
+        alert("Please enter a GitHub username");
+      }
 
 };
+
+var getLocationWeather = function(location) {
+     var apiUrl =  'https://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=f4d9020b0d30765266d6150cdd94784b'
+    
+    // make a get request to url
+  fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+        //displayRepos(data, user); CHANGE THIS *******************
+      });
+    } else {
+      alert('Error: City Not Found');
+    }
+  })
+  .catch(function(error) {
+    alert("Unable to connect to Weather API");
+  });
+};
+
 
 
 //Event Listener to btnClick 
